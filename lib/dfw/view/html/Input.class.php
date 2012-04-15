cc68d717af8f13373bee7d6c3276506d0b3401c2
Daniel Bonfim <daniel.fb88@gmail.com>
2012-04-15 10:38:29 -0300

    corrigido espaco entre as propriedades

--- lib/dfw/view/html/Input.class.php
+++ lib/dfw/view/html/Input.class.php
@@ -187,44 +187,44 @@ class Input extends Element {
         $input = '<input ';
         
         if(!empty($this->type))
-            $input .= ' type=\''.$this->type.'\' ';
+            $input .= 'type=\''.$this->type.'\' ';
         
         
         if(!empty($this->accept))
-            $input .= ' accept=\''.$this->accept.'\' ';
+            $input .= 'accept=\''.$this->accept.'\' ';
         
         if(!empty($this->accesskey))
-            $input .= ' accesskey=\''.$this->accesskey.'\' ';
+            $input .= 'accesskey=\''.$this->accesskey.'\' ';
         
         if(!empty($this->alt))
-            $input .= ' alt=\''.$this->alt.'\' ';
+            $input .= 'alt=\''.$this->alt.'\' ';
         
         if($this->checked)
-            $input .= ' checked=\'checked\' ';
+            $input .= 'checked=\'checked\' ';
         
         if(($this->type == 'radio' || $this->type == 'checkbox') && $this->disabled)
-            $input .= ' disabled=\'disabled\' ';
+            $input .= 'disabled=\'disabled\' ';
         
         if(!empty($this->maxlength))
-            $input .= ' maxlength=\''.$this->maxlength.'\' ';
+            $input .= 'maxlength=\''.$this->maxlength.'\' ';
         
         if($this->readonly)
-            $input .= ' readonly=\'readonly\' ';
+            $input .= 'readonly=\'readonly\' ';
         
         if(!empty($this->size))
-            $input .= ' size=\''.$this->size.'\' ';
+            $input .= 'size=\''.$this->size.'\' ';
         
         if($this->type == 'image' && !empty($this->src))
-            $input .= ' src=\''.$this->src.'\' ';
+            $input .= 'src=\''.$this->src.'\' ';
         
         if(!empty($this->tabindex))
-            $input .= ' tabindex=\''.$this->tabindex.'\' ';
+            $input .= 'tabindex=\''.$this->tabindex.'\' ';
         
         if(!empty($this->value))
-            $input .= ' value=\''.$this->value.'\' ';
+            $input .= 'value=\''.$this->value.'\' ';
         
         if(!empty($this->name))
-            $input .= ' name=\''.$this->name.'\' ';
+            $input .= 'name=\''.$this->name.'\' ';
         
         $input .= parent::show();
         
@@ -235,7 +235,7 @@ class Input extends Element {
         if(!empty($this->onfocus))
             $input .= ' onfocus=\''.$this->onfocus.'\' ';
         
-        $input .= ' />';
+        $input .= '/>';
                 
         echo $input;
     }
